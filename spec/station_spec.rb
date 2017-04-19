fdee265b6a5f49030237768c15aaa32af563d92e
JayWebDevCom <jaiye.s@gmail.com>
2017-04-19 21:45:17 +0100

    station rspec edit

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -1,7 +1,7 @@
 require 'station'
 
 describe Station do
-let(:station) { described_class.new("Aldgate", 2) }
+let(:subject) { described_class.new("Aldgate", 2) }
 
   it { is_expected.to respond_to(:name) }
   it { is_expected.to respond_to(:zone) }
