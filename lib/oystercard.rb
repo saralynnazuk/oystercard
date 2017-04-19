9e4e607c5e5d3a5f34d3d20a5e1be2b16744aa24
Sara Riley <saralynnaz@gmail.com>
2017-04-19 09:57:06 +0100

    Complete Challenge #09: Checking min balance on touch in

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,6 +1,7 @@
 class Oystercard
 
   MAXIMUM_BALANCE = 90
+  MINIMUM_BALANCE = 1
 
   attr_reader :balance, :in_journey
 
@@ -10,7 +11,7 @@ class Oystercard
   end
 
   def top_up(amount)
-    fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if amount + balance > MAXIMUM_BALANCE
+    fail "Maximum balance of £#{MAXIMUM_BALANCE} exceeded" if amount + balance > MAXIMUM_BALANCE
     @balance += amount
   end
 
@@ -19,10 +20,12 @@ class Oystercard
   end
 
   def in_journey?
-    @in_journey
+    !!@in_journey
   end
 
   def touch_in
+    p "my balance is:", balance
+    raise "Insufficient funds, please top up!" if balance < MINIMUM_BALANCE
     @in_journey = true
   end
 
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -10,33 +10,42 @@ describe Oystercard do
         expect { subject.top_up 1 }.to change { subject.balance }.by 1
       end
 
-      it 'raises and error if top up exceeds maximum balance' do
+      it 'raises an error if top up exceeds maximum balance' do
         maximum_balance = Oystercard::MAXIMUM_BALANCE
         subject.top_up(maximum_balance)
-        expect { subject.top_up 1 }.to raise_error "Maximum balance of #{maximum_balance} exceeded"
+        expect { subject.top_up 1 }.to raise_error "Maximum balance of £#{maximum_balance} exceeded"
       end
     end
 
     describe '#deduct' do
       it 'deducts fare from the balance' do
         subject.top_up(20)
-        expect { subject.deduct 1 }.to change { subject.balance }.by -1
+        expect { subject.deduct 3 }.to change { subject.balance }.by -3
       end
     end
 
-    it 'is initially not in a journey' do
-      expect(subject).not_to be_in_journey
+    describe '#in_journey' do
+      it 'is initially not in a journey' do
+        expect(subject).not_to be_in_journey
+      end
     end
 
     describe '#touch_in' do
       it 'can touch in' do
+        subject.top_up(2)
         subject.touch_in
         expect(subject).to be_in_journey
+      end
+
+      it 'will not allow touch in if below minimum balance' do
+        message = "Insufficient funds, please top up!"
+        expect { subject.touch_in }.to raise_error message
       end
     end
 
     describe '#touch_out' do
       it 'can touch out' do
+        subject.top_up(2)
         subject.touch_in
         subject.touch_out
         expect(subject).not_to be_in_journey
